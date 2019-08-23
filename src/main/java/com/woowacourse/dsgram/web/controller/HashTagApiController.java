9c5b2f887111afdfa116044f0204b8e2f3bb1c4a
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-24 01:36:45 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/com/woowacourse/dsgram/web/controller/HashTagApiController.java
+++ src/main/java/com/woowacourse/dsgram/web/controller/HashTagApiController.java
@@ -2,8 +2,6 @@ package com.woowacourse.dsgram.web.controller;
 
 import com.woowacourse.dsgram.service.HashTagService;
 import com.woowacourse.dsgram.service.dto.HashTagResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +15,6 @@ public class HashTagApiController {
     public HashTagApiController(HashTagService hashTagService) {
         this.hashTagService = hashTagService;
     }
-
-    private static final Logger log = LoggerFactory.getLogger(HashTagApiController.class);
 
     @GetMapping
     public ResponseEntity<HashTagResponse> searchHashTag(String query) {
