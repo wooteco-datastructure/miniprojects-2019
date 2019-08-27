d242ecd171f7af4e4f3e47c03fb0d47756b2f145
jinwook <jin942002@naver.com>
2019-08-27 16:03:39 +0900

    refactor: index.html에서 필요없는 script 문 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into user(id, email, nick_name, password, user_name) values(1, 'qwe@naver.com', 'qwe', 'qweqwe', 'qwe');
-#
-insert into file_info(id, file_name, file_path) values(1, '', '');
-#
+
+insert into file_info(id, file_name, file_path) values(1, 'eastjun_profile.jpg', '/Users/jinwook/woowa_course/level2/miniprojects-2019/src/main/resources/static/images/default');
+
 insert into article(id, contents, author, fileinfo_id) values(1, '1#qwe#qqq#qww#qee', 1, 1);
 insert into article(id, contents, author, fileinfo_id) values(2, '2#qwe#qqq', 1, 1);
 insert into article(id, contents, author, fileinfo_id) values(3, '3qwe', 1, 1);
@@ -21,15 +21,15 @@ insert into article(id, contents, author, fileinfo_id) values(16, '16#qwe', 1, 1
 insert into article(id, contents, author, fileinfo_id) values(17, '17#qwe', 1, 1);
 insert into article(id, contents, author, fileinfo_id) values(18, '18#qwe', 1, 1);
 insert into article(id, contents, author, fileinfo_id) values(19, '19#qwe', 1, 1);
-#
-#
+
+
 insert into hash_tag(id, keyword, article_id) values(1, '#qwe', 1);
 insert into hash_tag(id, keyword, article_id) values(2, '#qqq', 1);
 insert into hash_tag(id, keyword, article_id) values(3, '#qww', 1);
 insert into hash_tag(id, keyword, article_id) values(4, '#qee', 1);
 insert into hash_tag(id, keyword, article_id) values(5, '#qwe', 2);
 insert into hash_tag(id, keyword, article_id) values(6, '#qqq', 2);
-#
+
 insert into hash_tag(id, keyword, article_id) values(7, '#qwe', 4);
 insert into hash_tag(id, keyword, article_id) values(8, '#qwe', 5);
 insert into hash_tag(id, keyword, article_id) values(9, '#qwe', 6);
