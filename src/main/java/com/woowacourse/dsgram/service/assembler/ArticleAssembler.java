a354e6df4dbe62d693f2d15e3554d1f9248ec24d
imkimheejoo <jujudamdadi@naver.com>
2019-09-03 12:36:36 +0900

    chore: 주석제거

--- src/main/java/com/woowacourse/dsgram/service/assembler/ArticleAssembler.java
+++ src/main/java/com/woowacourse/dsgram/service/assembler/ArticleAssembler.java
@@ -5,17 +5,6 @@ import com.woowacourse.dsgram.domain.User;
 import com.woowacourse.dsgram.service.dto.article.ArticleInfo;
 
 public class ArticleAssembler {
-//    public static ArticleInfo toArticleInfo(Article article) {
-//        User author = article.getAuthor();
-//        return ArticleInfo.builder()
-//                .articleId(article.getId())
-//                .articleFileName(article.getFileInfo().getFileName())
-//                .contents(article.getContents())
-//                .nickName(author.getNickName())
-//                .userId(author.getId())
-//                .build();
-//    }
-
     public static ArticleInfo toArticleInfo(Article article) {
         User author = article.getAuthor();
         return ArticleInfo.builder()
